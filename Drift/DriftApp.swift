6250c986298c7d4f14131161afb6c9d0dbb34c47
cmillstein1 <cmillstein1@me.com>
2026-01-15 12:54:00 -0500

    Initial Commit

---
+++ Drift/DriftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriftApp.swift
+//  Drift
+//
+//  Created by Casey Millstein on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct DriftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
